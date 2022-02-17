c72b3ad45eed3a19581eb0429dfc8155bca9f1a0
Roberto Martins <roberto.martinsps3@gmail.com>
2022-02-17 20:29:13 -0300

    Primeira Versão

---
+++ Triângulos de Pascal.py
@@ -0,0 +1,28 @@
+from math import factorial as fatorial
+
+def combinacao(n, k):
+    numerador = fatorial(n)
+    divisor = (fatorial(k) * (fatorial(n - k)))
+    resultado = numerador // divisor
+    return resultado
+
+
+triangulo = []
+qtd_linhas = int(input('Digite o número da linha em que deseja que o triângulo termine: '))
+tamanho_justificado = len(str(combinacao(qtd_linhas, qtd_linhas // 2))) + 2
+for n in range(0, qtd_linhas + 1):
+    linha = []
+    for k in range(n + 1):
+        if k == 0:
+            linha.append('1'.rjust(tamanho_justificado))
+        elif k == 1:
+            linha.append(str(n).rjust(tamanho_justificado))
+        elif k == n:
+            linha.append('1'.rjust(tamanho_justificado))
+        else:
+            resultado_coeficiente = combinacao(n, k)
+            resutltado_formatado = str(resultado_coeficiente).rjust(tamanho_justificado)
+            linha.append(resutltado_formatado)
+    triangulo.append(linha)
+for linha in triangulo:
+    print(linha)
